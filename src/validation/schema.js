6dbfb655f38886c1efaff56c3126c242bc06ba0f
Balaji J <balajijanarthanan1997@gmail>
2024-08-17 11:25:40 +0530

    trial: added schemaValidation: userSchema

---
+++ src/validation/schema.js
@@ -0,0 +1,15 @@
+const Joi = require("joi");
+
+const userSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+function validateUserSchema(data) {
+  return userSchema.validate(data);
+}
+
+module.exports = {
+  validateUserSchema,
+};
